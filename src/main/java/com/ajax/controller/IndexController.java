e771853ad3afb2008c1da6ff780ef0b8a9dc4bf7
Park JW <p.jangwoo@gmail>
2023-03-13 18:41:08 +0900

    Ajax - 3월 13일

---
+++ src/main/java/com/ajax/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.ajax.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+	
+	@GetMapping("/")
+	public String index() {		//http://localhost:9696/	
+		return "index"; 
+	}
+	
+}
